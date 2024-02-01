1d8f32ad5d68b3b719ba07e632cf582cd16d2ea4
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2024-02-01 21:35:11 +0530

    Add files via upload

---
+++ Array deletion.cpp
@@ -0,0 +1,26 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int size,index,i;
+    cout<<"Enter the size of the array"<<endl;
+    cin>>size;
+    int ar[size];
+     for(i=0;i<size;i++)
+     {
+        cin>>ar[i];
+     }
+     cout<<"Enter the index of element to delete"<<endl;
+      cin>>index;
+     for(i=index;i<size;i++)
+     {
+        ar[i]=ar[i+1];
+     }
+      size--;
+      for(i=0;i<size;i++)
+      {
+        cout<<ar[i];
+      }
+      return 0;
+}
