b95d3c8caeedc21252d793a37361f69e0c021618
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2024-02-18 21:47:39 +0530

    Array insertion.cpp

--- Array insertion.cpp
+++ Array insertion.cpp
@@ -1,20 +1,23 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+
+int main() {
     int ar[10] = {1,2,3,4,5};
-    int position,num,i;
-    cout<<"Enter the position and number "<<endl;
-    cin>>position>>num;
-    for(i=4;i>=position;i--)
-    {
-        ar[i+1]=ar[i];
-        ar[position]=num;
+    int position, num, i;
+    
+    cout << "Enter the position and number: ";
+    cin >> position >> num;
+    
+    for(i = 4; i >= position; i--) {
+        ar[i + 1] = ar[i];
     }
-    cout<<"New array "<<endl;
-    for(i=0;i<6;i++)
-    {
-        cout<<ar[i];
+     // Insert the new element at the specified position
+    ar[position] = num;
+    
+    cout << "New array: ";
+    for(i = 0; i < 6; i++) {
+        cout << ar[i] << " ";
     }
+    
     return 0;
 }
