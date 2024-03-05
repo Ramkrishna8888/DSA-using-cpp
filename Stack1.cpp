bfda994363a33686d8ed79af73009c516c51e3d2
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2024-03-05 19:16:23 +0530

    Add files via upload

---
+++ Stack1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+   // Creating an stack
+   stack<int> n;
+   
+   // push operation
+   n.push(2);
+   n.push(3);
+
+   // pop
+   n.pop();
+
+   cout<<"Top element "<< n.top()<<endl;
+   
+   // using empty function for checking weather the stack is empty or not
+   if(n.empty())
+   {
+    cout<<"The stack is empty ";
+   }
+   else
+   {
+    cout<<"The stack is not empty ";
+   }
+return 0;
+
+}
