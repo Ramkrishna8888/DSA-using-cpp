1776a807e0d5c84e764d5c8299fdd53d2dd3a0d0
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2024-03-05 19:20:47 +0530

    Stack1.cpp

--- Stack1.cpp
+++ Stack1.cpp
@@ -7,13 +7,14 @@ int main()
    // Creating an stack
    stack<int> n;
    
-   // push operation
+   // push operation for insertion of element
    n.push(2);
    n.push(3);
 
-   // pop
+   // pop operation for the deletion of the element
    n.pop();
 
+   // Top operation is for checking the top element in the stack
    cout<<"Top element "<< n.top()<<endl;
    
    // using empty function for checking weather the stack is empty or not
