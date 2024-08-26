662f3ed0786bd889835bd0562f44ff9062a1c1bd
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2024-08-26 15:22:52 +0530

    Add files via upload
    this code contain the binary search program without oops concept just a simple logic

---
+++ binary_search4.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int arr[100];
+     int i,size;
+     cout<<"Enter the size of the array : ";
+     cin>>size;
+    for(i=0; i<size; i++)
+    {
+        cin>>arr[i];
+
+    }
+    int n;
+    cout<<"Enter the number to be searched : ";
+    cin>>n;
+    int start = 0;
+     int end = size-1;
+     int mid;
+     while(start <= end)
+     {
+        mid= (start + end)/2;
+        if(start<=end)
+        {
+        if(n==arr[mid])
+        {
+            cout<<"The element found  at index "<<mid;
+            
+        }
+        if(n<arr[mid])
+        {
+            end = mid-1;
+       }
+        else
+        {         
+             start = mid+1;
+        }
+     }
+     else
+         cout<<"The element is not present in the array ";
+     }
+   return 0;
+}
