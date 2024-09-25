b8459926d31f06d188915f4ea9644f3cf8082bb5
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2024-09-25 23:57:02 +0530

    Array_Move_zero_to_end.cpp

---
+++ Array_Move_zero_to_end.cpp
@@ -0,0 +1,46 @@
+# move zero at the end
+#saral logic mein
+#pehle non zero integers ko ek empty vector mein rakh lo fir bache hue place par zero bhar do dusre loop se
+# two pointer concept
+
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> zeroo (vector<int>arr, int n)
+{
+     vector<int> nums(n);
+     int j = 0;
+     for (int i = 0; i < n; i++)
+     {
+        if (arr[i]!=0)
+        {
+            nums[j] = arr[i];
+            j++;
+        }
+     }
+
+     for (int k = j; k < n; k++)
+     {
+         nums[k+1] = 0;
+     }
+
+       return nums;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int ele = 0;
+    vector<int> z;
+    for (int j = 0; j < n; j++)
+    {
+        cin>>ele;
+        z.push_back(ele);
+    }
+    vector<int> x = zeroo(z,n);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<x[i]<<" ";
+    }
+   return 0;
+}
