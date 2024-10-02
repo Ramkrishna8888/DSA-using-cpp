f29ac98922ed57fd06e0e02ac781a650833c7ef3
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2024-10-02 19:37:36 +0530

    set_uniqueElements.cpp

---
+++ set_uniqueElements.cpp
@@ -0,0 +1,26 @@
+# SIMPLE PROGRAM TO UNDERSTAND THE SET DATA STRUCTURE
+#include<iostream>
+ #include<set>
+ using namespace std;
+ int main()
+ {
+     set<int> unique_num;
+     int n,num=0;
+
+    cout<<"Enter the size "<<endl;
+    cin>>n;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>num;
+        unique_num.insert(num);
+    }
+
+    cout<<"Unique elements are "<<endl;
+
+    for(int it : unique_num)
+    {
+        cout<<it<<" ";
+    }
+   return 0;
+ }
