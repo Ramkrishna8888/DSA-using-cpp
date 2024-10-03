b36d72e3600fffa1644dff4996e7a6b7dc08f0b8
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2024-10-03 12:46:56 +0530

    Update Array_RemoveDuplicate.cpp
    added a full oops code the first version contains only the function snippet

--- Array_RemoveDuplicate.cpp
+++ Array_RemoveDuplicate.cpp
@@ -14,3 +14,42 @@ public:
            return (i+1);  // + 1 isliye taki actual count pta chal paye unique elemment kaa
     }
 };
+
+// FULL CODE 
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int removeDuplicates(vector<int>& arr, int n)
+{
+    int j=0;
+    for (int i = 1; i < n; i++)
+    {
+       if(arr[j]!=arr[i])
+       {
+        arr[j+1] = arr[i];
+        j++; 
+       }
+    }
+    return j+1;
+}
+int main()
+{
+    int size;
+    int num;
+    vector<int> arr;
+    cout<<"Enter the size of the array : "<<endl;
+    cin>>size;
+    for (int i = 0; i < size; i++)
+    {
+        cin>>num;
+        arr.push_back(num);
+    }
+   int ans = removeDuplicates(arr,size);
+   for (int k = 0; k < ans; k++)
+   {
+        cout<<arr[k]<<" ";
+   } 
+
+   cout<<endl<<"Unique emlements are : "<<ans;   
+}
